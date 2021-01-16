aaae25c01217f77d0565ab95690d7907c67ba336
coderharshmalik <harshmalik20015@gmail.com>
2021-01-16 23:40:42 +0530

    initial commit

---
+++ numberguess.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<time.h>
+using namespace std;
+int main()
+{
+    srand(time(0));
+    int a=rand()% 10;
+    int b;
+    int c=1;
+    cout<<"enter bw 0 and 10"<<" "<<endl;
+    while(c<=3)
+    {
+       cin>>b;
+       if(b>a){
+           cout<<"try lesser";
+       }
+       else if(b<a){
+           cout<<"try bigger";
+       }
+       c++;
+       if(a==b){
+           cout<<"you won";
+           break;
+       }
+       
+    }
+
+    return 0;
+}
